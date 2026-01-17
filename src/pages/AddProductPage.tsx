e9f378765e3227954137d76192a0df34e4b8cb3e
md sahin biswas <mdsahinbiswas420@gmail.com>
2026-01-17 19:06:50 +0600

    product add button complete

--- src/pages/AddProductPage.tsx
+++ src/pages/AddProductPage.tsx
@@ -1,31 +1,57 @@
-import { Button } from "antd";
-import { CopyOutlined, EyeOutlined } from "@ant-design/icons";
+import { Button, message } from "antd"; // message যোগ করা হয়েছে এলার্ট দেখানোর জন্য
+import { CopyOutlined, EyeOutlined, SaveOutlined } from "@ant-design/icons"; // SaveOutlined আইকন
 import ProductInformationComponent from "../components/addproduct/ProductInformationComponent";
 import PricingComponent from "../components/addproduct/PricingComponent";
 import OrganizationComponent from "../components/addproduct/OrganizationComponent";
 import MediaComponent from "../components/addproduct/MediaComponent";
 import VariantsComponent from "../components/addproduct/VariantsComponent";
+import { useNavigate } from "react-router"; 
 
 const AddProductPage = () => {
+  const navigate = useNavigate();
+
+  
+  const handleSave = () => {
+    
+    message.success("Product saved successfully!");
+    
+    navigate("/products"); 
+  };
+
   return (
     <div className="m-2">
       {/* header */}
-      <div className="bg-white  rounded-md px-6 py-4 shadow-sm">
-        <span>
-          <a href="" className="hover:underline text-gray-500">
-            Product
-          </a>
-          /Add Product
-        </span>
-        <h1 className="text-2xl font-semibold text-gray-90 p-1">Add Product</h1>
-        <div className="flex gap-3 mt-2">
-          <Button icon={<CopyOutlined />}>Duplicate</Button>
-          <Button icon={<EyeOutlined />}>Preview</Button>
+      <div className="bg-white rounded-md px-6 py-4 shadow-sm">
+        <div className="flex justify-between items-center">
+          <div>
+            <span>
+              <a href="" className="hover:underline text-gray-500">
+                Product
+              </a>
+              /Add Product
+            </span>
+            <h1 className="text-2xl font-semibold text-gray-90 p-1">Add Product</h1>
+          </div>
+          
+          {/* button section */}
+          <div className="flex gap-3 mt-2">
+            <Button icon={<CopyOutlined />}>Duplicate</Button>
+            <Button icon={<EyeOutlined />}>Preview</Button>
+            
+            <Button 
+              type="primary" 
+              icon={<SaveOutlined />} 
+              onClick={handleSave}
+              style={{ backgroundColor: '#1677ff' }}
+            >
+              Save Product
+            </Button>
+          </div>
         </div>
       </div>
+
       {/* main section */}
       <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
-
         <div className="col-span-4 mt-3">
           <div>
             <ProductInformationComponent />
@@ -50,4 +76,5 @@ const AddProductPage = () => {
     </div>
   );
 };
+
 export default AddProductPage;
